0c96d08e6791380cc37e5435f6377604394b9a8e
Eom Yejin <djaxl1121@naver.com>
2022-04-26 10:19:34 +0900

    실습 7-3

---
+++ Project7_3/app/src/main/java/com/cookandroid/project7_3/MainActivity.java
@@ -0,0 +1,63 @@
+package com.cookandroid.project7_3;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvName, tvEmail;
+    Button button1;
+    EditText dlgEdtName, dlgEdtEmail;
+    TextView toastText;
+    View dialogView, toastView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("사용자 정보 입력");
+
+        tvName = (TextView) findViewById(R.id.tvName);
+        tvEmail = (TextView) findViewById(R.id.tvEmail);
+        button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialogView = (View) View.inflate(MainActivity.this, R.layout.dialog1, null); // dialog1.xml파일을 inflate하여 dialogView에 대입
+                AlertDialog.Builder dlg = new AlertDialog.Builder(MainActivity.this);
+                dlg.setTitle("사용자 정보 입력");
+                dlg.setIcon(R.drawable.btn_star_big_on);
+                dlg.setView(dialogView); // 인플레이트한 뷰를 대화상자로 사용
+                dlg.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dlgEdtName = (EditText) dialogView.findViewById(R.id.dlgEdt1);
+                        dlgEdtEmail = (EditText) dialogView.findViewById(R.id.dlgEdt2);
+                        tvName.setText(dlgEdtName.getText().toString());
+                        tvEmail.setText(dlgEdtEmail.getText().toString());
+                    }
+                });
+                dlg.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast toast = new Toast(MainActivity.this);
+                        toastView = (View) View.inflate(MainActivity.this, R.layout.toast1, null); // toast1.xml파일을 inflate하여 toastView에 대입
+                        toastText = (TextView) toastView.findViewById(R.id.toastText1);
+                        toastText.setText("취소했습니다");
+                        toast.setView(toastView);
+                        toast.show();
+                    }
+                });
+                dlg.show();
+            }
+        });
+    }
+}
